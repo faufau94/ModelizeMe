249d9d3d16eff997f8b140c373461f75320cf6ea
faudel <faudelh94@hotmail.fr>
2024-11-11 19:56:01 +0100

    finish galery's page

--- server/api/models/list.get.ts
+++ server/api/models/list.get.ts
@@ -3,20 +3,44 @@ import {getServerSession} from "#auth";
 
 export default defineEventHandler(async event => {
 
-    const session = await getServerSession(event)
-
+    const session = await getServerSession(event);
 
     const getCurrentUser = await prisma.user.findUnique({
         where: {
             email: session?.user?.email,
         }
-    })
+    });
 
-    return await prisma.model.findMany({
-        where: {
-            userId: getCurrentUser.id
-        },
-    })
+    if (!getCurrentUser) {
+        throw new Error("Utilisateur non trouvé");
+    }
+
+    const query = getQuery(event);
+
+    if (query.onlyTemplates === 'true') {
+        // Récupérer uniquement les modèles associés à des galeries avec leurs catégories
+        return await prisma.model.findMany({
+            where: {
+                userId: getCurrentUser.id,
+                Galery: {
+                    some: {},
+                },
+            },
+            include: {
+                Galery: {
+                    include: {
+                        category: true,
+                    },
+                },
+            },
+        });
+    } else {
+        // Récupérer tous les modèles de l'utilisateur
+        return await prisma.model.findMany({
+            where: {
+                userId: getCurrentUser.id,
+            },
+        });
+    }
 
 });
-
