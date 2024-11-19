e6b223cd8b17f783bb646b6ae81494269225a765
faudel <faudelh94@hotmail.fr>
2024-11-19 07:45:53 +0100

    add process.env.URL_BACKEND inside download.post.ts

--- server/api/generator/download.post.ts
+++ server/api/generator/download.post.ts
@@ -6,7 +6,7 @@ export default defineEventHandler(async (event) => {
 
 
     try {
-        return await $fetch('http://modelizeme-generator.org/api/download-project', {
+        return await $fetch(process.env.URL_BACKEND + '/api/download-project', {
             method: 'POST',
             body: {
                 projectName
