1ebfb9e9b2f0af3d36955a59aade12ceaf2341b5
faudel <faudelh94@hotmail.fr>
2025-05-31 13:30:00 +0200

    feat: update Prisma schema and seed script; add user, session, account, organization, and member models, and refactor seeding logic for user creation

---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "~/prisma/generated/prisma/client";
+import { admin } from "better-auth/plugins";
+import { organization } from "better-auth/plugins";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "mysql",
+    }),
+    emailAndPassword: {  
+        enabled: true,
+        autoSignIn: false
+    },
+    plugins: [
+        admin(),
+        organization()
+    ]
+});
--- prisma/migrations/20240822225938_added_name_to_model/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `name` to the `models` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `models` ADD COLUMN `name` VARCHAR(191) NOT NULL,
-    MODIFY `content` JSON NULL;
